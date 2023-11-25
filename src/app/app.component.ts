6b83f175aad14c499d8ed0e1cb819d4d51cc3133
Reonido <faiteforever@gmail.com>
2023-11-25 17:12:03 +0200

    test api request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code-jam-ui';
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  public ngOnInit() {
+    this.http.get('http://localhost:3000/user').subscribe(console.log);
+  }
 }
