7b67d6707681c4aab0b544dc2b405e9698d6e1ad
Reonido <faiteforever@gmail.com>
2023-12-05 21:24:38 +0200

    Additional cleanup

--- proxy.conf.js
+++
@@ -1,9 +0,0 @@
-const defaultTarget = 'https://code-jam-serv-1c40d7e358ba.herokuapp.com';
-
-module.exports = [
-  {
-    context: ['/**'],
-    target: defaultTarget,
-    changeOrigin: true,
-  }
-];
